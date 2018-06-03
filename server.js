4bae9d00f2fa9ac5ae8d7fb523f504055033274e
Benoît Hubert <benoithubert@gmail.com>
2018-06-03 17:20:37 +0200

    Close the damn <ul>

--- server.js
+++ server.js
@@ -6,9 +6,11 @@ app.get('/', (req, res) => {
     'Content-Type': 'text/html',
   })
   .status(200)
-  .send(`<h1>Hello World</h1><ul>
-    <li><a href="/">Home (this page)</a></li>
-    <li><a href="/page-not-found">Show me a 404!</a></li>
+  .send(`<h1>Hello World</h1>
+    <ul>
+      <li><a href="/">Home (this page)</a></li>
+      <li><a href="/page-not-found">Show me a 404!</a></li>
+    </ul>
   `);
 });
 
